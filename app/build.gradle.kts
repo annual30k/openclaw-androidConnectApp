6f1763d4d9dc76cfe6b23d0fad96f307c37e511a
qiuqiquan <1363809106@qq.com>
2026-07-29 23:42:45 +0800

    chore: 发布 Android 1.0.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.rethinkingstudio.clawlink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.0.14"
+        versionCode = 18
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
