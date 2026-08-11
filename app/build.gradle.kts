3d20f9f7f90a1d2fe59605e75a41c196c3fa9862
qiuqiquan <1363809106@qq.com>
2026-08-11 19:42:54 +0800

    chore: 发布 Android 1.0.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.rethinkingstudio.clawlink"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.0.17"
+        versionCode = 21
+        versionName = "1.0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
